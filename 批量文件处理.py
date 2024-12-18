6a5842f786e23d856aba29bd3a2f0e2b7acff75b
HomePC <1553170687@qq.com>
2024-12-18 22:55:09 +0800

    [change]匹配第几集的正则表达式加一

---
+++ 批量文件处理.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# @Author  : 雷智杰
+# @Time    : 2024/10/20 16:27
+# @File    : 批量文件重命名
+# @Project : LifeTool
+# @Software: PyCharm
+
+
+import os
+import re
+import shutil
+
+
+def change_extension(directory, new_extension):
+    """
+    更改指定目录下的所有文件扩展名
+    :param directory: 路径
+    :param new_extension: 新的扩展名
+    :return:
+    """
+    # 遍历指定目录及其子目录下的所有文件
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # 获取文件的完整路径
+            file_path = os.path.join(root, file)
+            # 获取文件的扩展名
+            _, ext = os.path.splitext(file_path)
+            # 如果文件扩展名不是指定的后缀，则更改扩展名为指定的后缀
+            if ext.lower() != f'.{new_extension.lower()}':
+                # 构造新的文件名
+                new_file_path = os.path.splitext(file_path)[0] + f'.{new_extension}'
+                # 重命名文件
+                os.rename(file_path, new_file_path)
+                print(f'Renamed "{file_path}" to "{new_file_path}"')
+
+
+def move_files_to_parent(folder_path):
+    # 获取文件夹中的所有文件和子文件夹
+    for item in os.listdir(folder_path):
+        # 构建完整的文件或文件夹路径
+        item_path = os.path.join(folder_path, item)
+
+        # 检查是文件还是文件夹
+        if os.path.isfile(item_path):
+            # 如果是文件，直接移动到父文件夹
+            shutil.move(item_path, os.path.join(os.path.dirname(folder_path), item))
+            print(f'Moved "{item_path}" to parent folder')
+        elif os.path.isdir(item_path):
+            # 如果是文件夹，递归调用函数
+            move_files_to_parent(item_path)
+            # 检查文件夹是否为空，如果为空，则删除空文件夹
+            if not os.listdir(item_path):
+                os.rmdir(item_path)
+
+
+def rename_files(directory):
+    """
+    重命名指定目录下的所有文件
+    :param directory: 路径
+    :return: None
+    """
+
+    s_pattern = re.compile(r'S(\d+)')
+    e_pattern = re.compile(r'EP(\d+)')
+    e_pattern_ex = re.compile(r'E(\d+)')
+
+    base_name = os.path.basename(directory)
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            s_match = s_pattern.search(filename)
+            e_match = e_pattern.search(filename)
+            e_match_ex = e_pattern_ex.search(filename)
+
+            new_name = base_name
+            if s_match:
+                s_number = s_match.group(1)
+                new_name += f"第{s_number}季"
+            if e_match:
+                e_number = e_match.group(1)
+                new_name += f"第{e_number}集"
+            elif e_match_ex:
+                e_number_ex = e_match_ex.group(1)
+                new_name += f"第{e_number_ex}集"
+
+            if not new_name == base_name:
+                new_file_path = os.path.join(directory, new_name)
+                os.rename(file_path, new_file_path)
+                print(f'Renamed "{file_path}" to "{new_file_path}"')
+
+
+if __name__ == '__main__':
+    directory_path = r'C:\Users\void_pc\Downloads\三国演义'
+    rename_files(directory_path)
+
